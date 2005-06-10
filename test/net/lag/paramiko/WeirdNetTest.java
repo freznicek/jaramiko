2b5195e1cf1ac0de123c35b5c99019b6373c7ec1
Robey Pointer <robey@lag.net>
2005-06-09 22:38:48 -0700

    fix up demo-test to use new api and pkey auth

--- test/net/lag/paramiko/WeirdNetTest.java
+++ test/net/lag/paramiko/WeirdNetTest.java
@@ -4,6 +4,7 @@
 
 package net.lag.paramiko;
 
+import java.io.FileInputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import junit.framework.*;
@@ -20,22 +21,18 @@ public class WeirdNetTest
         throws Exception
     {
         Socket sock = new Socket();
-        sock.connect(new InetSocketAddress("localhost", 2200));
+        sock.connect(new InetSocketAddress("tavi.lag.net", 22));
         
         BaseTransport t = new BaseTransport(sock);
         t.setLog(new ConsoleLog());
         t.setDumpPackets(true);
-        Event e = new Event();
-        t.startClient(e);
-        
-        System.err.println("waiting for event...");
-        e.waitFor(10000);
-        if (! e.isSet()) {
-            System.err.println("never set. :(");
-        } else {
-            System.err.println("OK, connected!");
-            t.authPassword("robey", "foo");
-        }
-        
+        PKey key = PKey.readPrivateKeyFromStream(new FileInputStream("/home/robey/.ssh/id_rsa"), null);
+        t.startClient(PKey.createFromBase64(TAVI_HOST_KEY), 5000);
+        t.authPrivateKey("robey", key, 5000);
     }
+    
+    private static String TAVI_HOST_KEY =
+        "AAAAB3NzaC1yc2EAAAABIwAAAIEArLCLIphyfl3JDa34i7nQB5DYRa7v3kMFWtgGf8A" +
+        "EsQj/LVg+iRFi5pXitFeroz1H0sRnLGGI1rKBC0oxc+y8j/CXPR0B09Ch3sWjTFE2No" +
+        "KMOuhO2IOghLOp6dzdw3TBlaqNXj4q8qokjE9b3y2r1l+73NWsXPrkGqS5RlqoXGk=";
 }
